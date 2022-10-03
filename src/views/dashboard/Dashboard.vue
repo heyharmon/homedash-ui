27e9ca92040a215aefc8542f3743539d58d653e7
Ryan Harmon <heyharmon@gmail.com>
2022-10-03 15:10:32 -0600

    Update Dashboard.vue

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -14,7 +14,7 @@
           <!-- Greeting -->
           <div class="flex flex-column gap-sm margin-bottom-xl">
             <p class="color-contrast-medium text-md">Good morning</p>
-            <p class="text-xxxl">12:20 PM</p>
+            <p class="text-xxxl">12:21 PM</p>
             <p>Monday, October 3</p>
           </div>
           
@@ -104,11 +104,9 @@ onMounted(() => {
 
 <style lang="scss">
   #app {
-    background-color: #f5f5f5;
-    height: 100vh;
-    
+    background-color: #f5f5f5;    
     // background: linear-gradient(0deg, #eaf8ff 0%, #ffffff 100%);
-
+    height: 100vh;
     
     // filter: blur(8px);
     // background: url('http://placekitten.com/1920/1080');
